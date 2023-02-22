7eef5479a0689d20dba7852fc2a541f07ad127a7
Sophie B <105296740+SofiSoap@users.noreply.github.com>
2023-02-22 15:06:15 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function hideDisplay() {
+  document.getElementById("ref2").style.visibility = "hidden";
+}
+
+function showDisplay() {
+  document.getElementById("ref2").style.visibility = "visible";
+}
